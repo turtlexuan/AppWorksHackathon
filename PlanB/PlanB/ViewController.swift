b35fb30d238258e410711b2924be82e5efa07eaa
naiyu-wang <boo20750242@gmail.com>
2017-06-10 11:52:21 +0800

    start

---
+++ PlanB/PlanB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlanB
+//
+//  Created by 王迺瑜 on 2017/6/10.
+//  Copyright © 2017年 AppWorksHackathon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
