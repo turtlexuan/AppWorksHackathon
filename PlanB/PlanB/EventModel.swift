2d8d62b6f7a1c6e3b2fdac1ab460710acbdd32ae
劉洧熏 <mohocho@liuweixunde-MacBook-Air.local>
2017-06-11 09:00:30 +0800

    change file name

--- PlanB/PlanB/EventModel.swift
+++ PlanB/PlanB/EventModel.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+enum EventType {
+
+    case outDoor, movie, food, shopping, exercise
+
+}
+
+struct EventModel {
+
+    var eventTitle: String
+    var eventType: EventType
+    var description: String
+    var eventDate: String
+    var eventPlace: String
+    var eventLatitude: Double
+    var eventLongitude: Double
+    var tags: String
+    var user_id: String
+
+    init(eventTitle: String, eventType: EventType, description: String, eventDate: String,
+         eventPlace: String, eventLatitude: Double, eventLongitude: Double, tags: String,
+         user_id: String) {
+
+        self.eventTitle = eventTitle
+        self.eventType = eventType
+        self.description = description
+        self.eventDate = eventDate
+        self.eventPlace = eventPlace
+        self.eventLatitude = eventLatitude
+        self.eventLongitude = eventLongitude
+        self.tags = tags
+
+    }
+
+}
